c4e5b4e974ca98d299c0b12a2dbd228a5760c238
pfstudio8 <facuesquivel338@gmail.com>
2026-02-19 23:23:27 -0300

    Update Remera.js

--- js/Remera.js
+++ js/Remera.js
@@ -1,35 +1,66 @@
-export default class Remera {
-    constructor(nombre, precio, imagen, stockPorTalle) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-        this.stockPorTalle = stockPorTalle;
-    }
+console.log("JS funcionando");
+import Remera from "./Remera.js";
 
-    render() {
-        let botonesTalles = "";
+const contenedor = document.getElementById("contenedor");
 
-        for (let talle in this.stockPorTalle) {
-            let stock = this.stockPorTalle[talle];
-            let disabled = stock === 0 ? "disabled" : "";
+const remeras = [
+    new Remera("Oversize", 20000, "img/berserk_front.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+    }),
+    new Remera("Oversize", 20000, "img/berserk_back.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+        }),
+    new Remera("Oversize", 20000, "img/toji_front.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+    }),
+    new Remera("Oversize", 20000, "img/toji_back.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+    }),
+    new Remera("Oversize", 20000, "img/one_piece_front.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+    }),
+    new Remera("Oversize", 20000, "img/one_piece_back.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 0
+    }),
+    new Remera("Top mujer", 10000, "img/gojo_eye.jpeg", {
+        Talle_Unico: 1
+    }),
+    new Remera("Top mujer", 10000, "img/heart_red.jpeg", {
+        Talle_Unico: 1
+    }),
+    new Remera("BoxyFit", 18000, "img/fentanxl.jpeg", {
+        S: 1,
+        M: 0,
+        L: 1,
+        XL: 2
+    }),
+    new Remera("Remera Mujer Classic", 12000, "img/butterfly.jpeg", {
+        S: 0,
+        M: 0,
+        L: 0,
+        XL: 1
+    }),
+    
+];
 
-            botonesTalles += `
-                <button class="talle-btn" ${disabled}>
-                    ${talle}
-                </button>
-            `;
-        }
-
-        return `
-            <div class="card">
-                <img src="${this.imagen}" alt="${this.nombre}">
-                <h2>${this.nombre}</h2>
-                <p>$${this.precio}</p>
-
-                <div class="talles">
-                    ${botonesTalles}
-                </div>
-            </div>
-        `;
-    }
-}
+remeras.forEach(remera => {
+    contenedor.innerHTML += remera.render();
+});
